b49ca615ee3f6cbe220933c5bc5bd8486d683507
gavin <215742216@qq.com>
2017-04-10 17:55:13 +0800

    type

--- JSONToModel/HomeViewController.m
+++ JSONToModel/HomeViewController.m
@@ -8,8 +8,10 @@
 
 #import "HomeViewController.h"
 #import "NSString+DictionaryValue.h"
+#import <objc/runtime.h>
 
-#define DEFINE_NSSTRING_HEAD @"@property (nonatomic , copy) NSString *"
+#define DEFINE_HEAD_NSSTRING @"@property (nonatomic , copy) NSString *"
+#define DEFINE_HEAD_NSNUMBER @"@property (nonatomic , strong) NSNumber *"
 
 @interface HomeViewController ()
 
@@ -42,12 +44,27 @@
         return;
     }
     
-    NSArray *arrKeys = [dic allKeys];
+    [self dicFilter:dic];
+    
+}
+
+
+- (void)dicFilter:(NSDictionary *)dicPar{
+    NSArray *arrKeys = [dicPar allKeys];
+    
     NSMutableArray *models = [[NSMutableArray alloc] init];
     
     
     for (NSString *str in arrKeys) {
-        NSString *temp = [NSString stringWithFormat:@"%@%@%@",DEFINE_NSSTRING_HEAD,str,@";"];
+        NSString *name = [[[dicPar objectForKey:str] superclass] description];
+        NSString *temp;
+        
+        if ([name isEqualToString:@"NSString"]) {
+            temp = [NSString stringWithFormat:@"%@%@%@",DEFINE_HEAD_NSSTRING,str,@";"];
+        }else if ([name isEqualToString:@"NSNumber"]){
+            temp = [NSString stringWithFormat:@"%@%@%@",DEFINE_HEAD_NSNUMBER,str,@";"];
+        }
+        
         [models addObject:temp];
     }
     
@@ -62,13 +79,16 @@
     _outputTextView.string = result;
 }
 
+
+
+
 - (void)showErrorMessage:(NSString *)message{
-    NSAlert *alert = [[NSAlert alloc]init];           // 创建一个alert
-    [alert setMessageText:@"警告!"];           // 设置信息标题
-    [alert setInformativeText:message];    // 设置信息内容
-    [alert addButtonWithTitle:@"确定"];                // 添加第一个按钮以及按钮上显示的文字
-//    [alert addButtonWithTitle:@"Good!"];              // 添加第二个按钮以及按钮上显示的文字
-//    [alert addButtonWithTitle:@"NO!"];                // 添加第三个按钮以及按钮上显示的文字
+    NSAlert *alert = [[NSAlert alloc]init];                   // 创建一个alert
+    [alert setMessageText:@"警告!"];                          // 设置信息标题
+    [alert setInformativeText:message];                      // 设置信息内容
+    [alert addButtonWithTitle:@"确定"];                       // 添加第一个按钮以及按钮上显示的文字
+    //    [alert addButtonWithTitle:@"Good!"];               // 添加第二个按钮以及按钮上显示的文字
+    //    [alert addButtonWithTitle:@"NO!"];                 // 添加第三个按钮以及按钮上显示的文字
     [alert runModal];       // 弹出Alert，
 }
 
